2e33798e13bdf6e284248eed936821898fb49632
Demerdo <Seifmohamedwael97@gmail.com>
2020-07-02 14:47:36 +0200

    back

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
-var express = require('express')
-var cors = require('cors')
-var bodyParser = require('body-parser')
-var app = express()
-const mongoose = require('mongoose')
 
-// var QRCode = require('qrcode')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const app = express();
 
-// QRCode.toString('I am a pony!',  function (err, url) {
-//     console.log(url)
-// })
-
-var port = process.env.PORT || 5000
-
-app.use(bodyParser.json())
-app.use(cors())
-app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
-)
-
-const mongoURI = 'mongodb+srv://demerdo:salma@cluster0-q4xfo.mongodb.net/login?retryWrites=true&w=majority'
-
-mongoose
-    .connect(
-        mongoURI,
-        { useNewUrlParser: true }
-    )
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err))
 
 var Users = require('./routes/Users')
 
+
+
+// Connect to Mongo
+mongoose
+    .connect('mongodb+srv://demerdo:salma@cluster0-q4xfo.mongodb.net/login?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }) // Adding new mongo url parser
+    .then(() => console.log("MongoDB Connected..."))
+    .catch(err => console.log(err));
+// Express body parser
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+// Cors
+app.use(cors());
+// Serve static assets if in production
+
+
+// Entry point
+app.get("/", (req, res) => res.send(`<h1>Welcome to Beat the QR app</h1>`));
+// Use Routes
 app.use('/users', Users)
 
-app.listen(port, function () {
-    console.log('Server is running on port: ' + port)
-})
+
+
+// Wrong path
+app.use((req, res) =>
+    res.status(404).send(`<h1>Can not find what you're looking for</h1>`)
+);
+// Port
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
